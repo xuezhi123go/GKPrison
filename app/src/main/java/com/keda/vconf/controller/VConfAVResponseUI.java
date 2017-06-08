b82ab60ca8dc82181bdd28dad5a603552293dd8e
FangforFun <870431602@qq.com>
2017-06-08 10:18:25 +0800

    版本回退...

--- app/src/main/java/com/keda/vconf/controller/VConfAVResponseUI.java
+++ app/src/main/java/com/keda/vconf/controller/VConfAVResponseUI.java
@@ -6,13 +6,11 @@ package com.keda.vconf.controller;
  */
 
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -22,22 +20,14 @@ import android.widget.TextView;
 
 import com.gkzxhn.gkprison.R;
 import com.gkzxhn.gkprison.ui.activity.normal_activity.VideoFace;
-import com.gkzxhn.gkprison.utils.NomalUtils.SPUtil;
-import com.gkzxhn.gkprison.utils.NomalUtils.ToastUtil;
 import com.keda.sky.app.PcAppStackManager;
 import com.keda.vconf.manager.VConferenceManager;
 import com.kedacom.kdv.mt.api.Conference;
 import com.kedacom.kdv.mt.constant.EmConfProtocol;
 import com.kedacom.kdv.mt.constant.EmNativeConfType;
-import com.megvii.licensemanager.Manager;
-import com.megvii.livenessdetection.LivenessLicenseManager;
-import com.megvii.livenesslib.LivenessActivity2;
-import com.megvii.livenesslib.util.ConUtil;
 import com.pc.utils.NetWorkUtils;
 
 import java.io.IOException;
-
-import static com.gkzxhn.gkprison.constant.Constants.fileName;
 
 /**
   * 音视频应答
@@ -236,10 +226,11 @@ public class VConfAVResponseUI extends ActionBarActivity implements View.OnClick
 				finish();
 				break;
 
-		case R.id.refuse_response_btn:// 拒绝呼叫
-				acceptVconfCall(false, false);
-				finish();
-				break;
+            case R.id.refuse_response_btn:// 拒绝呼叫
+                hintResponseBtn();
+                acceptVconfCall(false, false);
+                finish();
+                break;
 
 			default:
 				break;
@@ -276,8 +267,8 @@ public class VConfAVResponseUI extends ActionBarActivity implements View.OnClick
 		super.onActivityResult(requestCode, resultCode, data);
 
 		if (resultCode == RESULT_OK && requestCode == ACTIVITY_REQUEST_CODE){
-			boolean verify = data.getBooleanExtra(LivenessActivity2.CONFIDENCE_RESULT, false);
-			double value = data.getDoubleExtra(LivenessActivity2.CONFIDENCE_VALUE, 0);
+			boolean verify = data.getBooleanExtra(VideoFace.CONFIDENCE_RESULT, false);
+			double value = data.getDoubleExtra(VideoFace.CONFIDENCE_VALUE, 0);
 			Log.i(TAG, "result : " + verify + ", value: " + value);
 			if (verify){
 				// 应答
@@ -383,53 +374,5 @@ public class VConfAVResponseUI extends ActionBarActivity implements View.OnClick
 		return mIsAudioConf;
 	}
 
-
-	/**
-	 * 人脸识别sdk授权验证任务
-	 */
-	class WarrantyTask extends AsyncTask<Void, Void, Integer> {
-
-		private ProgressDialog mProgressDialog = new ProgressDialog(VConfAVResponseUI.this);
-
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			mProgressDialog.setTitle("提示");
-			mProgressDialog.setMessage("正在联网授权中...");
-			mProgressDialog.setCanceledOnTouchOutside(false);
-			mProgressDialog.show();
-		}
-
-		@Override
-		protected Integer doInBackground(Void... params) {
-			Manager manager = new Manager(VConfAVResponseUI.this);
-			LivenessLicenseManager licenseManager = new LivenessLicenseManager(
-					VConfAVResponseUI.this);
-			manager.registerLicenseManager(licenseManager);
-			manager.takeLicenseFromNetwork(ConUtil.getUUIDString(VConfAVResponseUI.this));
-			if (licenseManager.checkCachedLicense() > 0)
-				return 1;
-			else
-				return 0;
-		}
-
-
-		@Override
-		protected void onPostExecute(Integer integer) {
-			super.onPostExecute(integer);
-			if (VConfAVResponseUI.this !=null&&!VConfAVResponseUI.this.isFinishing()){
-				mProgressDialog.dismiss();
-			}
-			if (integer == 1) {
-				Log.i(TAG, "人脸识别授权验证成功");
-				Intent intent = new Intent(VConfAVResponseUI.this, LivenessActivity2.class);
-				intent.putExtra(LivenessActivity2.UUID, (String) SPUtil.get(VConfAVResponseUI.this, "password", ""));
-				intent.putExtra(LivenessActivity2.IMAGE_REF_PATH, fileName);
-				startActivityForResult(intent, ACTIVITY_REQUEST_CODE);
-			} else if (integer == 0) {
-				ToastUtil.showShortToast("授权验证失败，请稍后再试！");
-			}
-		}
-	}
 
 }
